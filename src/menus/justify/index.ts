ab3ad42b8e9f1cd32d28c2618f0864246947f4cc
q378532364 <378532364@qq.com>
2020-07-08 20:40:12 +0800

    fix:修复icon与文字不对齐的情况

--- src/menus/justify/index.ts
+++ src/menus/justify/index.ts
@@ -58,8 +58,9 @@ class Justify extends DropListMenu implements MenuActive {
     }
 
     /**
-     * 尝试改变菜单激活（高亮）状态
+     * 尝试改变菜单激活状态
      * 默认左对齐,若选择其他对其方式对active进行高亮否则unActive
+     * ?考虑优化的话是否可以对具体选中的进行高亮
      */
     public tryChangeActive() {
         // const editor = this.editor
